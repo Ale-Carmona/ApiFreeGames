1c19e022a4b914007cdf513d006cea68646059aa
ALESSANDRA LEON CARMONA <112898@alumnouninter.mx>
2025-12-03 21:07:09 -0600

    cambio 1

--- ApiFreeGames/Models/AuthModel.cs
+++ ApiFreeGames/Models/AuthModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace ApiFreeGames.Models
 {
-    public class AuthModel
+    public class AuthModel  
     {
         public string? Username { get; set; }
         public string? Password { get; set; }
